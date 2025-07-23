f195a853ac4c4c68a973dcd56da18ea5f4654ccc
Your Name <enssyilmzx@gmail.com>
2025-07-24 01:07:25 +0300

    İletişim kısmı düzenlenildi ve animasyon için gerekli kütüphaneler eklendi.

---
+++ src/views/user/SkillsView.vue
@@ -0,0 +1,6 @@
+<template>
+  <section id="yetenekler" class="min-h-screen flex flex-col justify-center items-start gap-4">
+    <h1 class="text-5xl font-bold text-gray-800">YETENEKLERİM</h1>
+    <p class="text-lg text-gray-600">Yeteneklerini buraya yazabilirsin.</p>
+  </section>
+</template>
