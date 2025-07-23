a7bbbb17c35ef79686cd8baa655f420f7cf76214
Your Name <enssyilmzx@gmail.com>
2025-07-23 15:41:28 +0300

    Initial commit for fresh start

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,23 +2,7 @@
 export default {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
-    extend: {
-      colors: {
-        'yellow-sea': {
-          50: '#fef9ec',
-          100: '#fdedc8',
-          200: '#fad98d',
-          300: '#f7c152',
-          400: '#f5a623',
-          500: '#ef8611',
-          600: '#d3640c',
-          700: '#af440e',
-          800: '#8f3511',
-          900: '#752c12',
-          950: '#431505',
-        },
-      },
-    },
+    extend: {},
   },
   plugins: [],
 }
